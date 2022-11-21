6cb78d84d01ee1461595672d04194e941f3bd339
Alif Khaerudin <alifkha7@live.com>
2022-11-21 13:05:21 +0700

    Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,13 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Role;
+use App\Models\Team;
+use App\Models\User;
+use App\Models\Company;
+use App\Models\Employee;
+use App\Models\Responsibility;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +21,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // User::factory(70)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        
+        // Company::factory(10)->create();
+        // Team::factory(30)->create();
+        // Role::factory(50)->create();
+        // Responsibility::factory(200)->create();
+        // Employee::factory(1000)->create();
     }
 }
